57d4ac97e5416522c2e530c58f48c22859bddfac
NeRo <tazo.gorgadze.02@gmail.com>
2025-10-27 18:57:21 +0400

    implement user authentication and enhance Sidebar with dynamic user info

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,10 +1,11 @@
 "use server";
 
 import { ID, Query } from "node-appwrite";
-import { createAdminClient } from "../appwrite";
+import { createAdminClient, createSessionClient } from "../appwrite";
 import { appwriteConfig } from "../appwrite/config";
 import { parseStringify } from "../utils";
 import { cookies } from "next/headers";
+import { avatarPlaceholderUrl } from "@/constants";
 
 const getUserByEmail = async (email: string) => {
   const { databases } = await createAdminClient();
@@ -59,7 +60,7 @@ export const createAccount = async ({
       {
         email,
         fullName,
-        avatar: "https://avatars.githubusercontent.com/u/124599?v=4",
+        avatar: avatarPlaceholderUrl,
         accountId,
       }
     );
@@ -90,5 +91,26 @@ export const verifySecret = async ({
     return parseStringify({ sessionId: session.$id });
   } catch (error) {
     handleError(error, "Failed to verify OTP");
+  }
+};
+
+export const getCurrentUser = async () => {
+  try {
+    const { account, databases } = await createSessionClient();
+
+    const result = await account.get();
+
+    const user = await databases.listDocuments( 
+      appwriteConfig.databaseId,
+      appwriteConfig.usersCollectionId,
+      [Query.equal("accountId", result.$id)]
+    );
+
+    if (user.total <= 0) return null;
+
+    return parseStringify(user.documents[0]);
+
+  } catch (error) {
+    handleError(error, "Failed to get current user");
   }
 };
--- next.config.ts
+++ next.config.ts
@@ -16,6 +16,10 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+      },
+      {
+        protocol: "https",
         hostname: "cdn.pixabay.com",
       },
       {
