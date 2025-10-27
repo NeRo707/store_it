976ce54673c6521711cee61caae9d0e78d265d41
NeRo <tazo.gorgadze.02@gmail.com>
2025-10-27 17:08:52 +0400

    add InputOTP component and integrate it into OTPModal for enhanced OTP input functionality

--- components/OTPModal.tsx
+++ components/OTPModal.tsx
@@ -11,13 +11,23 @@ import {
 } from "@/components/ui/alert-dialog";
 import { Button } from "@/components/ui/button";
 
+import {
+  InputOTP,
+  InputOTPGroup,
+  InputOTPSeparator,
+  InputOTPSlot,
+} from "@/components/ui/input-otp";
+
 const OTPModal = ({
   email,
   accountId,
 }: {
   email: string;
   accountId: string;
-}) => {
+  }) => {
+  
+  console.log("OTPModal props:", { email, accountId });
+  
   return (
     <AlertDialog>
       <AlertDialogTrigger asChild>
@@ -31,6 +41,19 @@ const OTPModal = ({
             account and remove your data from our servers.
           </AlertDialogDescription>
         </AlertDialogHeader>
+        <InputOTP maxLength={6}>
+          <InputOTPGroup>
+            <InputOTPSlot index={0} />
+            <InputOTPSlot index={1} />
+            <InputOTPSlot index={2} />
+          </InputOTPGroup>
+          <InputOTPSeparator />
+          <InputOTPGroup>
+            <InputOTPSlot index={3} />
+            <InputOTPSlot index={4} />
+            <InputOTPSlot index={5} />
+          </InputOTPGroup>
+        </InputOTP>
         <AlertDialogFooter>
           <AlertDialogCancel>Cancel</AlertDialogCancel>
           <AlertDialogAction>Continue</AlertDialogAction>
