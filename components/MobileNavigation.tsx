7b112a44e6c20757f990f64f5576b4a50724a692
NeRo <tazo.gorgadze.02@gmail.com>
2025-11-01 13:19:27 +0400

    enhance AuthForm for user sign-in functionality and update MobileNavigation and Header to support user sign-out

--- components/MobileNavigation.tsx
+++ components/MobileNavigation.tsx
@@ -15,6 +15,7 @@ import Link from "next/link";
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import FileUploader from "@/components/FileUploader";
+import { signOutUser } from "@/lib/actions/user.actions";
 
 interface Props {
   $id: string;
@@ -24,11 +25,7 @@ interface Props {
   email: string;
 }
 
-const MobileNavigation = ({
-  fullName,
-  avatar,
-  email,
-}: Props) => {
+const MobileNavigation = ({ fullName, avatar, email }: Props) => {
   const [open, setOpen] = useState(false);
   const pathname = usePathname();
 
@@ -76,7 +73,7 @@ const MobileNavigation = ({
                   <li
                     className={cn(
                       "mobile-nav-item",
-                      pathname === url && "shad-active",
+                      pathname === url && "shad-active"
                     )}
                   >
                     <Image
@@ -86,7 +83,7 @@ const MobileNavigation = ({
                       height={24}
                       className={cn(
                         "nav-icon",
-                        pathname === url && "nav-icon-active",
+                        pathname === url && "nav-icon-active"
                       )}
                     />
                     <p>{name}</p>
@@ -103,7 +100,7 @@ const MobileNavigation = ({
             <Button
               type="submit"
               className="mobile-sign-out-button"
-              onClick={() => { }}
+              onClick={async () => await signOutUser()}
             >
               <Image
                 src="/assets/icons/logout.svg"
